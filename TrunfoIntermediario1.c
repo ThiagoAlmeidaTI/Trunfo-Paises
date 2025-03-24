2383cfd3610c56abde8ceffa3ec8e39fe3b3483d
Seu Nome <seu-email@dominio.com>
2025-03-24 19:26:01 -0300

    Editando

--- TrunfoIntermediario1.c
+++ TrunfoIntermediario1.c
@@ -6,27 +6,33 @@ int main() {
     long populacao1, populacao2;
     double area1, area2;
     long pib1, pib2;
-    int pontos_turísticos1, pontos_turísticos2;
+    int pontos_turisticos1, pontos_turisticos2;
     float densidade1, densidade2, pib_per_capita1, pib_per_capita2;
 
     // Entrada dos dados para a primeira cidade
     printf("Digite o estado da primeira cidade (A-H): ");
     scanf(" %c", &estado1);
+    printf("Estado da primeira cidade recebido: %c\n", estado1); // Mensagem de depuração
 
     printf("Digite o número da cidade (1-4): ");
     scanf(" %d", &cidade_num1);
+    printf("Número da cidade recebido: %d\n", cidade_num1); // Mensagem de depuração
 
     printf("Digite a população da cidade: ");
     scanf("%ld", &populacao1);
+    printf("População da cidade recebida: %ld\n", populacao1); // Mensagem de depuração
 
     printf("Digite a área da cidade (em km²): ");
     scanf("%lf", &area1);
+    printf("Área da cidade recebida: %.2f\n", area1); // Mensagem de depuração
 
     printf("Digite o PIB da cidade (em R$): ");
     scanf("%ld", &pib1);
+    printf("PIB da cidade recebido: %ld\n", pib1); // Mensagem de depuração
 
     printf("Digite o número de pontos turísticos da cidade: ");
-    scanf("%d", &pontos_turísticos1);
+    scanf("%d", &pontos_turisticos1);
+    printf("Número de pontos turísticos recebido: %d\n", pontos_turisticos1); // Mensagem de depuração
 
     // Cálculo da densidade populacional e PIB per capita para a primeira cidade
     densidade1 = populacao1 / area1;
@@ -35,21 +41,27 @@ int main() {
     // Entrada dos dados para a segunda cidade
     printf("\nDigite o estado da segunda cidade (A-H): ");
     scanf(" %c", &estado2);
+    printf("Estado da segunda cidade recebido: %c\n", estado2); // Mensagem de depuração
 
     printf("Digite o número da cidade (1-4): ");
     scanf(" %d", &cidade_num2);
+    printf("Número da cidade recebido: %d\n", cidade_num2); // Mensagem de depuração
 
     printf("Digite a população da cidade: ");
     scanf("%ld", &populacao2);
+    printf("População da cidade recebida: %ld\n", populacao2); // Mensagem de depuração
 
     printf("Digite a área da cidade (em km²): ");
     scanf("%lf", &area2);
+    printf("Área da cidade recebida: %.2f\n", area2); // Mensagem de depuração
 
     printf("Digite o PIB da cidade (em R$): ");
     scanf("%ld", &pib2);
+    printf("PIB da cidade recebido: %ld\n", pib2); // Mensagem de depuração
 
     printf("Digite o número de pontos turísticos da cidade: ");
-    scanf("%d", &pontos_turísticos2);
+    scanf("%d", &pontos_turisticos2);
+    printf("Número de pontos turísticos recebido: %d\n", pontos_turisticos2); // Mensagem de depuração
 
     // Cálculo da densidade populacional e PIB per capita para a segunda cidade
     densidade2 = populacao2 / area2;
@@ -62,7 +74,7 @@ int main() {
     printf("População: %ld\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: R$ %.2f bilhões\n", pib1 / 1000000000.0);  // Exibindo o PIB em bilhões
-    printf("Número de Pontos Turísticos: %d\n", pontos_turísticos1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
     printf("PIB per Capita: R$ %.2f\n", pib_per_capita1);
 
@@ -72,7 +84,7 @@ int main() {
     printf("População: %ld\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: R$ %.2f bilhões\n", pib2 / 1000000000.0);  // Exibindo o PIB em bilhões
-    printf("Número de Pontos Turísticos: %d\n", pontos_turísticos2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
     printf("PIB per Capita: R$ %.2f\n", pib_per_capita2);
 
